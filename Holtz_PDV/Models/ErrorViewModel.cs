3b64099dd771258fbac3f9dd9d86012906497d1d
Henrique Holtz <henrique_holtz@hotmail.com>
2020-08-23 09:24:39 -0300

    Initial commit

---
+++ Holtz_PDV/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Holtz_PDV.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
